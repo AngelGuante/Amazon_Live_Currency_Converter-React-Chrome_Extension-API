e5365605fb512c54868b1db91eb9d3223742f566
Miguel Angel Guante Suero <49294128+AngelGuante@users.noreply.github.com>
2023-07-29 00:44:55 -0400

    Adding cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const dop_uds = require('./Services/GoogleFinanceScrapping')
+const cors = require('cors')
 const express = require('express')
 const app = express()
 
@@ -6,6 +7,10 @@ app.listen(3000, () => {
     console.log('listening on port 3000')
 })
 
+module.exports = cors({
+    origin: '*',
+})
+
 app.get('/', async (_, res) => {
-    res.json({'dop_usd': Number(await dop_uds())});
+    res.json({ 'dop_usd': Number(await dop_uds()) });
 })
